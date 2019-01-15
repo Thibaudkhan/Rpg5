739b7556a03745a1340463ef318bba3ed6b2f43c
Unknown <thibaud.roussel@edu.itescia.fr>
2019-01-15 17:32:27 +0100

    Code plus opti et verification

--- src/fr/groupe4/cours/personnage/Personnage.java
+++ src/fr/groupe4/cours/personnage/Personnage.java
@@ -9,6 +9,8 @@ public class Personnage {
     Map<String, Integer> map = new HashMap<>();
     int points = 30;
     int valeur = 0;
+    Scanner sc = new Scanner(System.in);
+
 
     public Personnage() {
 
@@ -29,54 +31,40 @@ public class Personnage {
             String entree;
             System.out.println("Que voulez-vous modifier ?");
             Scanner scanner = new Scanner(System.in);
-            Scanner sc = new Scanner(System.in);
             entree = scanner.nextLine();
-            System.out.println("Vous avez " + points);
+            System.out.println("Vous avez " + points + "points");
 
             switch (entree) {
                 case "Force":
 
-                    System.out.println("Combien voulez-vous assigner de points à force ?");
-                    valeur = Integer.valueOf(sc.nextLine());
-                    verifValeur(valeur, points);
-                    map.replace("Force", valeur);
-                    points -= valeur;
-                    break;
+
+                        mettreCaract("Force");
+                        break;
 
                 case "Intelligence":
 
-                    System.out.println("Combien voulez-vous assigner de points à intelligence ?");
-                    valeur = Integer.valueOf(sc.nextLine());
-                    verifValeur(valeur, points);
-                    map.replace("Intelligence", valeur);
-                    points -= valeur;
-                    break;
+
+
+                        mettreCaract("Intelligence");
+                        break;
+
 
                 case "Dextérité":
 
-                    System.out.println("Combien voulez-vous assigner de points à dextérité ?");
-                    valeur = Integer.valueOf(sc.nextLine());
-                    verifValeur(valeur, points);
-                    map.replace("Dextérité", valeur);
-                    points -= valeur;
+
+                    mettreCaract("Dextérité");
                     break;
 
                 case "Sagesse":
 
-                    System.out.println("Combien voulez-vous assigner de points à sagesse ?");
-                    valeur = Integer.valueOf(sc.nextLine());
-                    verifValeur(valeur, points);
-                    map.replace("Sagesse", valeur);
-                    points -= valeur;
+
+                    mettreCaract("Sagesse");
                     break;
 
                 case "Constitution":
 
-                    System.out.println("Combien voulez-vous assigner de points à constitution ?");
-                    valeur = Integer.valueOf(sc.nextLine());
-                    verifValeur(valeur, points);
-                    map.replace("Constitution", valeur);
-                    points -= valeur;
+
+                    mettreCaract("Constitution");
                     break;
 
                 default:
@@ -89,15 +77,28 @@ public class Personnage {
         }
         System.out.println(map);
     }
-    public int
 
-    /*
     public int verifValeur (int valeur, int points) {
         if (valeur >= points) {
             this.valeur = this.points;
         }
         return valeur;
-    }*/
+
+    }
+    public int mettreCaract (String valeurCaract ) {
+
+            System.out.println("Combien voulez-vous assigner de points à " + valeurCaract + "?");
+            valeur = Integer.valueOf(sc.nextLine());
+        if (valeur >= 8 && valeur <= 15) {
+            verifValeur(valeur, points);
+            map.replace(valeurCaract, valeur);
+            points -= valeur;
+
+        }else{
+            System.out.println("Veuillez recommencer");
+        }
+        return valeur;
+    }
 
     /*public int setElement(String carac, int trucEnPlus) {
         switch (carac) {
