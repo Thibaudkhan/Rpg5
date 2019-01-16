e203806ded1fe0247a54f72166f49552bc1864d9
Unknown <thibaud.roussel@edu.itescia.fr>
2019-01-16 09:12:06 +0100

    joli joli

--- src/fr/groupe4/cours/personnage/Personnage.java
+++ src/fr/groupe4/cours/personnage/Personnage.java
@@ -10,24 +10,18 @@ public class Personnage {
     int points = 30;
     int valeur = 0;
     Scanner sc = new Scanner(System.in);
-
-
     public Personnage() {
-
 
         map.put("Force", 0);
         map.put("Intelligence", 0);
         map.put("Dextérité", 0);
         map.put("Sagesse", 0);
         map.put("Constitution", 0);
-
         setCarac();
-
     }
 
     public void setCarac() {
         while (points > 0) {
-
             String entree;
             System.out.println("Que voulez-vous modifier ?");
             Scanner scanner = new Scanner(System.in);
@@ -36,33 +30,22 @@ public class Personnage {
 
             switch (entree) {
                 case "Force":
-
-
                     mettreCaract("Force");
                     break;
 
                 case "Intelligence":
-
-
                     mettreCaract("Intelligence");
                     break;
 
-
                 case "Dextérité":
-
-
                     mettreCaract("Dextérité");
                     break;
 
                 case "Sagesse":
-
-
                     mettreCaract("Sagesse");
                     break;
 
                 case "Constitution":
-
-
                     mettreCaract("Constitution");
                     break;
 
@@ -104,34 +87,4 @@ public class Personnage {
         }
         return valeur;
     }
-
-    /*public int setElement(String carac, int trucEnPlus) {
-        switch (carac) {
-            case "Force":
-                force = caracteristique.put("Force", trucEnPlus);
-                System.out.println(force);
-                return force;
-
-            case "Intelligence":
-                intell = caracteristique.put("Int", trucEnPlus);
-                System.out.println("Hello Case intell");
-                return intell;
-
-            case "Dextérité":
-                dexterite = caracteristique.put("Dext", trucEnPlus);
-                return dexterite;
-
-            case "Sagesse":
-                sagesse = caracteristique.put("Sag", trucEnPlus);
-                return sagesse;
-
-            case "Constitution":
-                constitution = caracteristique.put("Const", trucEnPlus);
-                return constitution;
-
-            default:
-                return 0;
-        }
-
-    }*/
 }
